d7ccd538ed4dd69610c6fc3ccc38a5491fee0c67
ShallyBanh <banh.shally@gmail.com>
2017-10-28 10:58:02 -0600

    added comments to habitEvent class

--- app/src/main/java/main/habitivity/habits/HabitEvent.java
+++ app/src/main/java/main/habitivity/habits/HabitEvent.java
@@ -15,31 +15,71 @@ public class HabitEvent {
 
     }
 
+    /**
+    * Gets the id of the HabitEvent
+    *
+    * @return id - id of the HabitEvent
+    */
     public String getId() {
-        return id;
+        return this.id;
     }
 
+    /**
+    * Sets the id of the HabitEvent
+    *
+    * @param [in] id - id of the HabitEvent
+    */
     public void setId(String id) {
     }
 
+    /**
+    * Gets the comment of the HabitEvent
+    *
+    * @return comment - comment of the HabitEvent
+    */
     public String getComment() {
-        return comment;
+        return this.comment;
     }
 
+    /**
+    * Sets the comment of the HabitEvent
+    *
+    * @param [in] comment - comment of the HabitEvent
+    */
     public void setComment(String comment) {
     }
 
+    /**
+    * Gets the completion date of the HabitEvent (i.e the date the habit was completed and became a "HabitEvent")
+    *
+    * @return completionDate - date that HabitEvent was created
+    */
     public Date getCompletionDate() {
-        return completionDate;
+        return this.completionDate;
     }
 
+    /**
+    * Sets the completion date of the HabitEvent (i.e the date the habit was completed and became a "HabitEvent")
+    *
+    * @param [in] completionDate - date that HabitEvent was created
+    */
     public void setCompletionDate(Date completionDate) {
     }
 
+    /**
+    * Gets the photo associated with the HabitEvent
+    *
+    * @return photograph - photo associated with the HabitEvent
+    */
     public Image getPhoto() {
-        return photograph;
+        return this.photograph;
     }
 
+    /**
+    * Sets the photo associated with the HabitEvent
+    *
+    * @param [in] photograph - photo associated with the HabitEvent
+    */
     public void setPhoto(Image photograph) {
     }
 
