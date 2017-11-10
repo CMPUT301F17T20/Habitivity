9969ce9451a8ab422f2262d2d46652d7b55af972
paranickcodez <nparada@ualberta.ca>
2017-11-09 22:21:42 -0700

    Recycleview listing each pending/requests/followers/
    The recycleview with coordinator layouts for the currents users follows that pend on the followees whitelisting, those followers of the user not whitelisted/approved by the user and those whitelisted with the option to remove/deny/blacklist

--- app/src/main/java/main/habitivity/FollowRequest.java
+++ app/src/main/java/main/habitivity/FollowRequest.java
@@ -7,8 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import main.habitivity.debug.R;
-
 public class FollowRequest extends AppCompatActivity {
 
     @Override
