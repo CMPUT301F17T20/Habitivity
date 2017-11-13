c9bce16f9d2651cd3b991aaf14e71d507c94a6d0
SethBergen <sbergen@ualberta.ca>
2017-11-13 02:07:21 -0700

    Implemented Today's Habits screen

--- app/src/main/java/main/habitivity/TodaysHabits.java
+++ app/src/main/java/main/habitivity/TodaysHabits.java
@@ -1,16 +1,117 @@
 package main.habitivity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class TodaysHabits extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import main.habitivity.habits.Habit;
+import main.habitivity.habits.HabitRepository;
+import main.habitivity.controllers.AddHabitController;
+import main.habitivity.controllers.AddHabitRequest;
+
+public class TodaysHabits extends BaseActivity {
+
+    private HabitListAdapter adapter;
+    private ArrayList<Habit> habitList;
+    private HabitRepository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todays_habits);
+        resolveDependencies();
+        habitList = repository.getHabits();
+        Habit h1 = new Habit();
+        h1.setTitle("HABIT_001");
+        Habit h2 = new Habit();
+        h2.setTitle("HABIT_002");
+        habitList.add(h1);
+        habitList.add(h2);
+
+        adapter = new HabitListAdapter(this, R.layout.list_item_habit_today, habitList);
+        ListView habitListDisplay = (ListView) findViewById(R.id.todays_habits);
+
+        habitListDisplay.setAdapter(adapter);
 
         //setEnabled(false)
 
+    }
+
+    private void resolveDependencies() {
+        HabitApplication app = getHabitApplication();
+        repository = app.getHabitRepository();
+    }
+
+    /**
+     * This class will allow us to customize the view for the habits in the list
+     * Such customization could include any android UI element (TextView, Button, etc)
+     *
+     * @author Seth Bergen
+     * @version 1.0
+     * @see android.widget.ArrayAdapter
+     * @see Habit
+     * @since 1.0
+     */
+    private class HabitListAdapter extends ArrayAdapter<Habit>{
+        private int layout;
+
+        /**
+         * Constructs HabitListAdapter objects
+         *
+         * @param context Target activity
+         * @param resource Layout of each list item
+         * @param objects List of objects to display
+         */
+        private HabitListAdapter(Context context, int resource, List<Habit> objects){
+            super(context, resource, objects);
+            layout = resource;
+        }
+
+        /**
+         * Builds a view for the list item
+         *
+         * @param position Index of list item
+         * @param convertView View used for each list item
+         * @param parent Parent activity
+         * @return convertView
+         */
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent){
+            HabitView mainHabitView = null;
+
+            if (convertView == null){
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                HabitView habitView = new HabitView();
+
+                habitView.habitDetails = (TextView) convertView.findViewById(R.id.textbox_habitDetails);
+
+                convertView.setTag(habitView);
+            }
+            mainHabitView = (HabitView) convertView.getTag();
+            mainHabitView.habitDetails.setText(habitList.get(position).toString());
+
+            return convertView;
+        }
+    }
+
+    /**
+     * Holds the TextView and any additional UI elements for each Habit in habitList
+     *
+     * @author Seth Bergen
+     * @version 1.0
+     * @since 1.0
+     */
+    private class HabitView{
+        TextView habitDetails;
     }
 }
